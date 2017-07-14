b96bcb3dc462b119e9eae13636698796e94353d3
Itay Cohen <itayco@il.ibm.com>
2017-07-14 16:30:47 +0300

    fix layout 40

--- components/playerPanel/playerComponent/dpYoutubeEmbedComponent.js
+++ components/playerPanel/playerComponent/dpYoutubeEmbedComponent.js
@@ -255,7 +255,7 @@ function dpYoutubeEmbedController($scope, dpSongsListLogic, $mdMedia) {
 	var playerwidthReducerFactor = 0.5; // factor that decide how wide the player width can be
 	var MINIMUM_WIDTH_OF_SCREEN = 768;
 
-	var maxPlayerWidth = 800;
+	var maxPlayerWidth = 850;
 
 	//hooking the dpSongsListLogic on logicService for html access
     $scope.logicService = dpSongsListLogic;
