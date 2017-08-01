0675bff724be77874ca65f45e9f5d1b9b3a35b75
Itay Cohen <itayco@il.ibm.com>
2017-08-01 19:01:11 +0300

    adding feedback - app

--- app.js
+++ app.js
@@ -8,9 +8,18 @@ var app = angular.module('dynamicPlaylistApp', [
     'dpGenreWidgetManagerComponent',
     'dpGenreWidgetComponent',
     'dpDynamicPlaylist'
-]); 
+]);
 
 app.run(['$rootScope', 'dpSongsListLoaderService', 'dpSongsListLogic', function ($rootScope, dpSongsListLoaderService, dpSongsListLogic) {
+
+    // RoadmapSettings.load({
+    //     id: "597f7f5a864eaf57933fae86" // find on Settings page, Widget tab
+    //     // email: "itay2002@gmail.com" // email of current user
+    //     // first: user.first, // first name of current user
+    //     // last: user.last, // last name of current user
+    //     // revenue: parseInt(user.planAmount), // MRR of current user
+    // });
+
     $rootScope.rawSongsList = dpSongsListLoaderService.getSongsList();
     dpSongsListLogic.initCalcSongsList();
 }]);
@@ -22,12 +31,29 @@ app.config(function ($mdThemingProvider) {
 });
 
 app.controller('appController', appController);
-appController.$inject = ["$scope", "$mdMedia"];
-function appController($scope, $mdMedia) {
+appController.$inject = ["$scope", "$mdMedia", "$mdDialog"];
+function appController($scope, $mdMedia, $mdDialog) {
+
     $scope.getMainViewClass = function () {
         if (!$mdMedia('max-width: 959px')) {
             return "main-view-big";
         }
+    };
+
+    $scope.openFeedbackForm = function (ev) {
+        $mdDialog.show({
+            // controller: DialogController,
+            templateUrl: 'components/app/feedbackForm.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            fullscreen: false // Only for -xs, -sm breakpoints.
+        })
+            .then(function (answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function () {
+                $scope.status = 'You cancelled the dialog.';
+            });
     };
 }
 
