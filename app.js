9af57fe2714c4841d06981a562390f5ec3e4ec96
Itay Cohen <itayco@il.ibm.com>
2017-08-21 19:23:21 +0300

    push

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var app = angular.module('dynamicPlaylistApp', [
 ]);
 
 app.run(['$rootScope', 'dpSongsListLoaderService', 'dpSongsListLogic', function ($rootScope, dpSongsListLoaderService, dpSongsListLogic) {
-
     dpSongsListLoaderService.loadSongsList().then(
         function (data) {
             $rootScope.rawSongsList = data;
